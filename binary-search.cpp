577325195888cb5e35a4254d9d0531adab436aa2
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2023-07-26 17:05:27 +0600

    binary-search updated

--- binary-search.cpp
+++ binary-search.cpp
@@ -2,53 +2,51 @@
 
 using namespace std;
 
-int binarySearch(vector<int> x, int target)
+int binarySearch(vector<int> &numbers, int target)
 {
-    int low = 0, high = x.size() - 1;
+    int low = 0, high = numbers.size() - 1;
     while (low <= high)
     {
         int mid = (high + low) / 2;
-        if (x[mid] == target) return mid;
-        else if (x[mid] < target) low = mid + 1;
+        if (numbers[mid] == target) return mid;
+        else if (numbers[mid] < target) low = mid + 1;
         else high = mid - 1;
     }
     return -1;
 }
 //... Time Complexity: O[log(n)]
 
-int lowerBound(vector<int> x, int target)
+int lowerBound(vector<int> &numbers, int target)
 {
-    int low = 0, high = x.size() - 1;
+    int low = 0, high = numbers.size() - 1;
     while (high - low > 1)
     {
         int mid = (high + low) / 2;
-        if (x[mid] < target) low = mid + 1;
+        if (numbers[mid] < target) low = mid + 1;
         else high = mid;
     }
-    if (x[low] >= target) return low;
-    else if (x[high] >= target) return high;
+    if (numbers[low] >= target) return low;
+    else if (numbers[high] >= target) return high;
     else return -1;
 }
 //... Time Complexity: O[log(n)]
 
-int upperBound(vector<int> x, int target)
+int upperBound(vector<int> &numbers, int target)
 {
-    int low = 0, high = x.size() - 1;
+    int low = 0, high = numbers.size() - 1;
     while (high - low > 1)
     {
         int mid = (high + low) / 2;
-        if (x[mid] <= target) low = mid + 1;
+        if (numbers[mid] <= target) low = mid + 1;
         else high = mid;
     }
-    if (x[low] > target) return low;
-    else if (x[high] > target) return high;
+    if (numbers[low] > target) return low;
+    else if (numbers[high] > target) return high;
     else return -1;
 }
 //... Time Complexity: O[log(n)]
 
 int main()
 {
-    vector<int> numbers = {1, 4, 7, 69, 74};
-    int target = 7;
-    cout << binarySearch(numbers, target);
+    return 0;
 }
