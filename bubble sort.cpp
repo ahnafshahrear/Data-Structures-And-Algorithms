f59600589417c4166292dec0da3c2a4376690213
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2023-08-16 16:43:11 +0600

    bubble sort added

---
+++ bubble sort.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void bubbleSort(vector<int> &numbers)
+{
+    //... Outer loop shrinks the size of the array from end
+    for (int i = 0; i < numbers.size(); i++)
+    {
+        //... Inner loop moves the largest element at the last index
+        for (int j = 1; j < numbers.size() - i; j++)
+        {
+            //... Swap if previous element is greater than current element
+            if (numbers[j - 1] > numbers[j])
+            {
+                swap(numbers[j - 1], numbers[j]);
+            }
+        }
+    }
+}
+
+int main()
+{
+    int size;
+    vector<int> numbers(size);
+    
+    //... Call bubbleSort(numbers) to sort "numbers" vector
+    bubbleSort(numbers);
+
+    return 0;
+}
