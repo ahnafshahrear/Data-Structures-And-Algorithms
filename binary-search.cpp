2ff99c864e936decb71ecc5220f5504195854bc6
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2024-04-10 04:09:06 +0600

    binary-search updated

--- binary-search.cpp
+++ binary-search.cpp
@@ -5,21 +5,33 @@ using namespace std;
 int binarySearch(vector<int> &numbers, int target)
 {
     int low = 0, high = numbers.size() - 1;
-    while (low <= high)
+
+    while (high - low > 1)
     {
         int mid = (high + low) / 2;
         if (numbers[mid] == target) return mid;
         else if (numbers[mid] < target) low = mid + 1;
         else high = mid - 1;
     }
-    return -1;
+
+    if (numbers[low] == target) return low;
+    else if (numbers[high] == target) return high;
+    else return -1;
 }
 //... Time Complexity: O[log(n)]
 
 int main()
 {
-    vector<int> numbers;
-    numbers = {24, 31, 74, 101, 143, 254};
-    int target = 101;
+    int size, target;
+    cin >> size >> target;
+    vector<int> numbers(size);
+
+    for (int i = 0; i < size; i++)
+    {
+        cin >> numbers[i];
+    }
+
     cout << binarySearch(numbers, target);
+
+    return 0;
 }
