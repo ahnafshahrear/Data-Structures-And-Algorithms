d0f5478568199f4a24645f75ca5cc1df63d3550f
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2024-04-10 18:06:32 +0600

    prime-test added

---
+++ Number Theory/prime-test.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool isPrime(int number)
+{
+	if (number <= 1) return false;
+
+	for (int n = 2; n * n <= number; n++)
+	{
+		if (number % n == 0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+//... Time Complexity: O[log(n)]
+
+int main()
+{
+	int number;
+	cin >> number;
+	
+	cout << isPrime(number);
+
+	return 0;
+}
