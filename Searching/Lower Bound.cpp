5238e19725516f1e3db8e7452ce005a476d5e9cf
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2024-07-01 18:09:36 +0600

    Lower Bound added

---
+++ Searching/Lower Bound.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int lowerBound(vector<int>& numbers, int target) {
+    int low = 0, high = numbers.size() - 1;
+
+    while (high - low > 1) {
+        int mid = (high + low) / 2;
+
+        if (numbers[mid] < target) {
+            low = mid + 1;
+        } else {
+            high = mid;
+        }
+    }
+
+    if (numbers[low] >= target) {
+        return low;
+    } else if (numbers[high] >= target) {
+        return high;
+    } else {
+        return -1;
+    }
+}
+
+int main() {
+    vector<int> numbers = {1, 3, 7, 12, 27, 35, 47, 54, 69, 71};
+
+    int target = 13;
+
+    cout << lowerBound(numbers, target);
+
+    return 0;
+}
