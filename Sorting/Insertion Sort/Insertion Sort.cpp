fe1923f6106fcf420a83a18f2ba14a82291761b2
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2024-07-02 22:39:30 +0600

    Insertion Sort added

---
+++ Sorting/Insertion Sort/Insertion Sort.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void sort(vector<int>& numbers) {
+    // Number before ith index is already sorted after each iteration
+    for (int i = 1; i < numbers.size(); i++) {
+        int k = i; // The numbers[k] will be inserted before kth index which is already sorted
+        // Inserting numbers[k] before kth index in the appropriate position
+        while (k > 0 and numbers[k] < numbers[k - 1]) {
+            swap(numbers[k], numbers[k - 1]);
+            k--;
+        }
+    }
+} // Time Complexity is O[n*n]
+
+int main() {
+    int size;
+    cin >> size;
+
+    vector<int> numbers(size);
+
+    for (int i = 0; i < size; i++) {
+        cin >> numbers[i];
+    }
+    sort(numbers);
+
+    for (int i = 0; i < size; i++) {
+        cout << numbers[i] << " ";
+    }
+
+    return 0;
+}
