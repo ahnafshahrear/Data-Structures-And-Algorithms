7e7a264196f7c11fc2aecc66a4015cec501699a7
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2024-08-01 15:32:59 +0600

    Selection Sort updated

--- Sorting/Selection Sort/Selection Sort.cpp
+++ Sorting/Selection Sort/Selection Sort.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 
 void selectionSort(vector<int>& numbers) {
-    // The numbers before ith position will be sorted after each iteration
-    // Next smallest number will be stored in the ith position
+    //... The numbers before i'th position is already sorted
+    //... The next smallest number will be stored in the i'th position
     for (int i = 0; i < numbers.size() - 1; i++) {
-        int minIndex = i; // Index of the next smallest number
+        int minIndex = i; //... Index of the next smallest number
         for (int j = i + 1; j < numbers.size(); j++) {
             if (numbers[j] < numbers[minIndex]) {
                 minIndex = j;
@@ -14,17 +14,22 @@ void selectionSort(vector<int>& numbers) {
         }
         swap(numbers[i], numbers[minIndex]);
     }
-} // Time Complexity is O[n*n]
+}
+//... Time Complexity is O[n^2] where n is the size of the array 
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int size;
-    cin >> size;
+    cin >> size; //... Input the size of the array
 
     vector<int> numbers(size);
 
     for (int i = 0; i < size; i++) {
-        cin >> numbers[i];
+        cin >> numbers[i]; //... Input the numbers
     }
+
     selectionSort(numbers);
 
     for (int i = 0; i < size; i++) {
@@ -32,3 +37,7 @@ int main() {
     }
     return 0;
 }
+
+//... Ahnaf Shahrear Khan, Computer Science & Engineering, University of Rajshahi
+//... Gmail: ahnafshahrearkhan@gmail.com
+//... GitHub: https://github.com/ahnafshahrear
