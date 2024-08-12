48c5ff6c6201a40a0848ab33357138224afc880c
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2024-08-12 22:18:26 +0600

    Counting Sort added

---
+++ Sorting/Counting Sort/Counting Sort.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void countingSort(vector<int>& numbers) {
+    int largest = 0; //... The largest element of the input
+
+    for (int n : numbers) {
+        largest = max(largest, n);
+    }
+    vector<int> count(largest + 1, 0); //... To store the frequency of each element
+
+    for (int n : numbers) {
+        count[n]++;
+    }
+    int i = 0;
+
+    for (int n = 0; n <= largest; n++) {
+        for (int k = 0; k < count[n]; k++) {
+            numbers[i++] = n;
+        }
+    }
+}
+//... Time Complexity is O[n + m] where n and m are the size of input & count array 
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int size;
+    cin >> size; //... Input the size of the array
+
+    vector<int> numbers(size);
+
+    for (int i = 0; i < size; i++) {
+        cin >> numbers[i]; //... Input the numbers
+    }
+
+    countingSort(numbers);
+
+    for (int i = 0; i < size; i++) {
+        cout << numbers[i] << " ";
+    }
+    return 0;
+}
+
+//... Ahnaf Shahrear Khan, Computer Science & Engineering, University of Rajshahi
+//... Gmail: ahnafshahrearkhan@gmail.com
+//... GitHub: https://github.com/ahnafshahrear
