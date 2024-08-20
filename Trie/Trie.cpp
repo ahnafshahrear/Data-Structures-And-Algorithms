478a135b24bcc6ce6a8977d004242a6e786880bc
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2024-08-20 11:51:59 +0600

    Trie updated

--- Trie/Trie.cpp
+++ Trie/Trie.cpp
@@ -2,32 +2,39 @@
 
 using namespace std;
 
-struct TreeNode {
-    TreeNode* links[26];
-    bool end = false;
+//... Struct TrieNode represents a node in a Trie data structure.
+struct TrieNode {
+    TrieNode* links[26]; //... Pointers to child nodes corresponding 'a' to 'z'
+    bool end = false; //... Flag to indicate the end of a word
 
-    TreeNode() {
+    TrieNode() {
+        //... Initialize all child pointers to nullptr
         for (int i = 0; i < 26; i++) {
             links[i] = nullptr;
         }
     }
 
+    //... Checks if the current node contains a child node corresponding to the given character
     bool containsKey(char ch) {
         return links[ch - 'a'] != nullptr;
     }
 
+    //... Creates a new child node corresponding to the given character
     void setKey(char ch) {
-        links[ch - 'a'] = new TreeNode;
+        links[ch - 'a'] = new TrieNode;
     }
 
-    TreeNode* getKey(char ch) {
+    //... Retrieves the child node corresponding to the given character
+    TrieNode* getKey(char ch) {
         return links[ch - 'a'];
     }
 
+    //... Marks the current node as the end of a word
     void setEnd() {
         end = true;
     }
 
+    //... Checks if the current node is end of a word
     bool isEnd() {
         return end;
     }
@@ -35,14 +42,15 @@ struct TreeNode {
 
 class Trie {
 private:
-    TreeNode* root;
-    TreeNode* node;
+    TrieNode* root;
+    TrieNode* node; 
 
 public:
     Trie() {
-        root = new TreeNode;
+        root = new TrieNode;
     }
 
+    //... Inserts a word into the Trie
     void insert(string word) {
         node = root;
 
@@ -52,9 +60,10 @@ public:
             }
             node = node->getKey(ch);
         }
-        node->setEnd();
+        node->setEnd(); //... Mark the end of the word
     }
 
+    //... Searches for an exact match of the word in the Trie
     bool search(string word) {
         node = root;
 
@@ -64,9 +73,10 @@ public:
             }
             node = node->getKey(ch);
         }
-        return node->isEnd();
+        return node->isEnd(); //... Check if the final node is the end of a word
     }
 
+    //... Checks if there is any word in the Trie that starts with the given word as a prefix
     bool startsWith(string word) {
         node = root;
 
@@ -76,19 +86,27 @@ public:
             }
             node = node->getKey(ch);
         }
-        return true;
+        return true; //... Means all characters are found
     }
 };
 
 int main() {
-    Trie trie;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    trie.insert("ahnafshahrear");
+    Trie trie; //... An object of the Trie class
 
-    cout << trie.search("ahnaf");
+    trie.insert("ahnafshahrear"); //... Inserting a word into the trie
+
+    cout << trie.search("ahnaf"); //... Exact word search
     cout << trie.search("ahnafshahrear");
 
-    cout << trie.startsWith("ahnaf");
+    cout << trie.startsWith("ahnaf"); //... Prefix search
 
     return 0;
 }
+
+//... Ahnaf Shahrear Khan
+//... Department of Computer Science & Engineering, University of Rajshahi
+//... Gmail: ahnafshahrearkhan@gmail.com
+//... GitHub: https://github.com/ahnafshahrear
